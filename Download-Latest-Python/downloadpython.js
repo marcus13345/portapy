02cd201a8316ce9457e9203d40b2aa94576018d2
Marcus <marcus@mandworks.com>
2018-10-30 18:31:40 -0400

    node_moduiles pls

--- Download-Latest-Python/downloadpython.js
+++ Download-Latest-Python/downloadpython.js
@@ -1,19 +1,50 @@
 let request = require('request');
-let https = require('https');
 let fs = require('fs');
 let os = require('os');
-let cmd = require('node-command-line');
+let {execSync} = require('child_process');
 let path = require('path');
-let waitOn = require('wait-on');
 
 request({
 	uri: "https://www.python.org/downloads/",
-
 }, function (error, response, body) {
 	
-	let os_string = os.platform();
+	let file_name = getPythonDownloadLink();
+
+	fs.mkdirSync('Python-Installer');
+
+	let out_file = fs.createWriteStream(`Python-Installer/${file_name}`);
+
+	
+	request({
+		uri: current_download,
+	}, function (error, response, body) {
+		fs.writeFileSync(out_file, body);
+	});
+	
+	console.log('Download Complete!');
+
+	switch (os.platform())
+	{
+		case 'win32':
+			try {
+				execSync(`${path.join(__dirname, 'Python-Installer/', file_name)} DefaultJustForMeTargetDir=${path.join(__dirname, '/../', 'Python/')}`, {
+					stdio: "inherit"
+				});
+
+				console.log("Python Installed!");
+			} catch (err) {
+				console.log(err);
+			}
+			break;
+	}
+	
+});
+
+
+function getPythonDownloadLink() {
+	
 	let current_os = '';
-	switch (os_string)
+	switch (os.platform())
 	{
 		case 'darwin':
 			current_os = "download-os-mac-osx";
@@ -28,47 +59,9 @@ request({
 			current_os = "download-os-source";
 			break;
 	}
-
-
+	
 	let current_download_start = body.indexOf("href=",body.indexOf(current_os)) + 6;
 	let current_download_end = body.indexOf('">', current_download_start);
 	let current_download = body.substring(current_download_start, current_download_end);
-
-	// function downlaod_python()
-	// {
-	//     return new Promise(function (resolve, reject) {
-			let file_name = current_download.substring(current_download.indexOf('/', current_download.indexOf('/python/') + 8) + 1);
-			// console.log(file_name);
-			let out_file = fs.createWriteStream(`../Python-Installer/${file_name}`);
-		
-			let file_request = https.get(current_download, function (response) {
-				response.pipe(out_file);
-			});
-			// resolve("Python Downloaded");
-	//     })
-	// }
-
-	// function install_python()
-	// {
-		// return new Promise(function (resolve, reject)
-		// {
-			switch (os_string)
-			{
-				case 'win32':
-					try {
-						cmd.run(`${path.join(__dirname, '/../', 'Python-Installer/', file_name)} /quiet DefaultJustForMeTargetDir=${path.join(__dirname, '/../', 'Python/')}`);
-						resolve("Python Installed");
-					} catch (err) {
-						console.log(err);
-					}
-					break;
-			}
-	//     })
-	// }
-	// async function main()
-	// {
-	//     await downlaod_python();
-	//     await install_python();
-	//     console.log("Done");
-	// }
-});
+	return file_name = current_download.substring(current_download.indexOf('/', current_download.indexOf('/python/') + 8) + 1);
+}
--- postinstall.js
+++ postinstall.js
@@ -1,9 +1,11 @@
 // pip install --target=lib numpy six pypng pyplatec
 let {execSync} = require('child_process');
 
-execSync('"Python/Scripts/pip.exe" install --target=worldengine/lib numpy six pypng pyplatec', {
-	stdio: 'inherit'
-});
+
+
+// execSync('"Python/Scripts/pip.exe" install --target=worldengine/lib numpy six pypng pyplatec', {
+// 	stdio: 'inherit'
+// });
 
 // execSync('"Python/Scripts/pip.exe" install --target=worldengine/lib numpy six pypng pyplatec', {
 // 	stdio: 'inherit'
