0fb1c80a282058ae2b33ea6e9e896dd38af0dd74
Marcus <marcus@mandworks.com>
2018-10-30 19:56:29 -0400

    idle works

--- bin/python.js
+++ bin/python.js
@@ -3,7 +3,7 @@
 const path = require('path');
 let pythonPath = path.resolve(__dirname, '..', 'Python', 'python.exe');
 
-console.log(process.argv);
+// console.log(process.argv);
 
-require('child_process').spawn(pythonPath, process.argv.slice(1), {stdio: 'inherit'});
+require('child_process').spawn(pythonPath, [], {stdio: 'inherit'});
 
