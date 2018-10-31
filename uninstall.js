adefc7bfc59a60334abc2a6c8f230c95e617ef42
Marcus <marcus@mandworks.com>
2018-10-30 20:19:30 -0400

    0.0.2

--- uninstall.js
+++ uninstall.js
@@ -1,5 +1,6 @@
 const path = require('path');
-let pythonPath = path.resolve(__dirname, 'Python', 'python.exe');
+let pythonPath = path.resolve(__dirname, 'Python-Installer', 'python-3.7.1.exe');
 
-require('child_process').exec(`${pythonPath} /passive /uninstall`);
-
+require('child_process').execSync(`${pythonPath} /passive /uninstall`, {
+	stdio: 'inherit'
+});
