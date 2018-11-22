3f5ecd706f3569bba1562f50b6fec30510cb6a8f
Valerie <marcus13345@live.com>
2018-11-21 20:51:22 -0500

    poopfart

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
 
+const minimist = require('minimist');
+console.log(minimist(process.args));
+
 let args = process.argv.slice(2);
 
 if (args[0] == 'help' || args[0] == '?' || args[0] == 'h'){
