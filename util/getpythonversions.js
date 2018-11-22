87a12a5a71dc558d8e3b2fd03cc1f2f0d96fa780
BrOakes <BrOakesGames@gmail.com>
2018-11-21 20:42:39 -0500

    portapy commands

--- util/getpythonversions.js
+++ util/getpythonversions.js
@@ -3,18 +3,20 @@ module.exports = {
 }
 const request = require('request');
 
-function versions()
-{
-	return new Promise(resolve => {
+// let matches = [];
+
+let versions = function() {
+	
+	new Promise(resolve => {
 		request({
 			uri: "https://www.python.org/ftp/python/",
 		}, function (error, response, body) {
 			const re = /"[0-9]{1,}\.[0-9]{1,}(\.[0-9]{1,})*\/"/g;
-			let matches = body.match(re)
+			matches = body.match(re)
 			matches = matches.map((value) => {
-				return value.slice(1,-2)
+				return value.slice(1, -2);
 			});
-				resolve(matches);
+			resolve(matches);
 		});
 	})
 }
